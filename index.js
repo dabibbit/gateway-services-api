28693bdbeb94e95ef48148314c1b905d8e2ded25
Abiy Seifu <abiy@ripple.com>
2014-09-05 11:44:54 -0700

    [FIX] pass gatewayd properly, add /bridge endpoint

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const BridgesController = require(__dirname+'/lib/bridges_controller.js');
 
 function Plugin(options) {
 
   var router = new express.Router();
-  var bridgesController = new BridgesController(options);
+  var bridgesController = new BridgesController({
+    gatewayd: options.gatewayd
+  });
 
-  router.post('/bridge/quotes', bridgesController.getQuote);
-  router.post('/bridge/payments', bridgesController.submitPayment);
-  router.get('/bridge/payments', bridgesController.paymentsHistory);
-  router.get('/bridge/payments/:id', bridgesController.paymentStatus);
+  router.get('/bridge', bridgesController.info.bind(bridgesController));
+  router.post('/bridge/quotes', bridgesController.getQuote.bind(bridgesController));
+  router.post('/bridge/payments', bridgesController.submitPayment.bind(bridgesController));
+  router.get('/bridge/payments', bridgesController.paymentsHistory.bind(bridgesController));
+  router.get('/bridge/payments/:id', bridgesController.paymentStatus.bind(bridgesController));
+
 
   this.router = router;
 }
